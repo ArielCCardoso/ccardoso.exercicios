4b99ed9e2328657a737ed0db92ba08e7e29b189f
Ariel C. Cardoso <arielccardoso@live.com>
2020-06-23 01:09:24 -0300

    Primeiro commit

---
+++ src/IntToArray/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IntToArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] num = Console.ReadLine().ToCharArray();
+            string[] s = new string[] { "zero", "um", "dois", "tres", "quatro", "cinco", "seis", "sete", "oito", "nove" };
+            Console.WriteLine();
+            for (int x=0; x<num.Length;x++)
+            {
+                string c = (num[x] % 2 == 0) ? "PAR" : "IMPAR";
+                Console.WriteLine($"A posição é o {x} e o número é {num[x]} ({s[int.Parse(num[x].ToString())]}) que é {c}.");
+            }
+        }
+    }
+}
