78988702ffe07707dd38052ba285a102b9fcbd9f
Ariel C. Cardoso <arielccardoso@live.com>
2020-07-16 23:36:32 -0300

    console teste de numero randominco

---
+++ src/NumeroRandomico/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NumeroRandomico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int x = r.Next(10000000, 99999999);
+            int y = r.Next(10000000, 99999999);
+            int z = r.Next(10000000, 99999999);
+            Console.WriteLine($"{x}\n{y}\n{z}");
+        }
+    }
+}
