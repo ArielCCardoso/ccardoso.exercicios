c08263a134bd2e43b5bf5e853be6873b1f1f7a8c
Ariel C. Cardoso <arielccardoso@live.com>
2020-08-03 21:42:45 -0300

    Adiciona projeto testes valor e referencia

---
+++ src/TipoValorTipoReferencia/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TipoValorTipoReferencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teste t1, t2, t3;
+            string s = new string("");
+            t1 = new Teste("Ariel", "Monte Santo", 99999);
+            t2 = t1;
+            t2.Set("Ariel", "Sao Paullo", 1000000000);
+            t3 = new Teste("Ariel", "Monte Santo");
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine($"{t1}\n{t2}\n{t3}");
+        }
+    }
+}
---
+++ src/TipoValorTipoReferencia/Teste.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TipoValorTipoReferencia
+{
+    class Teste
+    {
+        public string Nome { get; private set; }
+        public string Cidade { get; private set; }
+        private double saldo;
+        public double Saldo { get { return this.saldo; } set { saldo = value; } }
+
+        public Teste(string nome, string cidade)
+        {
+            Nome = nome;
+            Cidade = cidade;
+        }
+        public Teste(string nome, string cidade, double saldo) : this (nome, cidade)
+        {
+            this.saldo = saldo;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome}, cidade: {Cidade}, Saldo: R$ {saldo}...";
+        }
+        public void Set(string a, string b, double c)
+        {
+            Nome = a;
+            Cidade = b;
+            Saldo = c;
+        }
+    }
+}
