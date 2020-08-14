f46059b8f25345348b6861370a608943e373b68e
Ariel C. Cardoso <arielccardoso@live.com>
2020-08-14 02:31:22 -0300

    Exercicios de fixação

---
+++ src/Composition2/Entities/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace Curso.Udemy.CSharpCompleto2020.Capitulo9.Composition2.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
